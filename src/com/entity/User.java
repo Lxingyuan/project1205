3d95c1aff788dd2fb60a2ff9d7d867f191cb5e5d
huaibbfjb <1254961614@qq.com>
2020-12-05 10:36:11 +0800

    Initial commit

---
+++ src/com/entity/User.java
@@ -0,0 +1,17 @@
+package com.entity;
+
+/**
+ * @author ：liuyuntao
+ * @date ：Created in 2020/12/5 10:32
+ */
+public class User {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
