be766779c63280512e91c7ced286473475fa4cbf
huaibbfjb <1254961614@qq.com>
2020-12-09 20:00:33 +0800

    20201209 刘云涛 20:00 描述:短信maven

---
+++ src/com/test/Test1.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import com.aliyuncs.exceptions.ClientException;
+import com.utils.SmsUtil;
+import org.junit.Test;
+
+/**
+ * @author ：liuyuntao
+ * @date ：Created in 2020/12/9 19:55
+ */
+public class Test1 {
+    @Test
+    public void t1() throws ClientException {
+        SmsUtil.sendSms("15277323599","1234");
+    }
+}
