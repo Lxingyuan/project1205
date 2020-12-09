759a9d59af6a41dfa24f8f3a74699b58a93c19cc
Admin <771357867@qq.com>
2020-12-09 14:51:45 +0800

    星源提交1209

---
+++ src/com/entity/Message.java
@@ -0,0 +1,90 @@
+package com.entity;
+
+
+/**
+ * 作者：LiuYunTao
+ * 日期: 15:37 2020/11/26
+ * 描述：
+ */
+public class Message {
+    private Integer id;
+    private Integer fromUid;
+    private Integer toUid;
+    private String mTitle;
+    private String mContent;
+    private Integer readFlag;
+    private String createTime;
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", fromUid=" + fromUid +
+                ", toUid=" + toUid +
+                ", mTitle='" + mTitle + '\'' +
+                ", mContent='" + mContent + '\'' +
+                ", readFlag=" + readFlag +
+                ", createTime='" + createTime + '\'' +
+                '}';
+    }
+
+    public Message() {
+    }
+
+    public Integer getFromUid() {
+        return fromUid;
+    }
+
+    public void setFromUid(Integer fromUid) {
+        this.fromUid = fromUid;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public Integer getToUid() {
+        return toUid;
+    }
+
+    public void setToUid(Integer toUid) {
+        this.toUid = toUid;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmContent() {
+        return mContent;
+    }
+
+    public void setmContent(String mContent) {
+        this.mContent = mContent;
+    }
+
+    public Integer getReadFlag() {
+        return readFlag;
+    }
+
+    public void setReadFlag(Integer readFlag) {
+        this.readFlag = readFlag;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
