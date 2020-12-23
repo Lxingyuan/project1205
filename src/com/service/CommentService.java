83c91f12e3b7ffc09f9d019b0dff9f998916fff3
huaibbfjb <1254961614@qq.com>
2020-12-23 20:19:03 +0800

    20201223 刘云涛 描述:删除评论

--- src/com/service/CommentService.java
+++ src/com/service/CommentService.java
@@ -20,7 +20,7 @@ public interface CommentService {
 
     //删除评论信息
     int deleteComment(Integer commentId);
-
+    int deleteComment(Integer movieId,String commentUser);
     List<Comment> queryCommentByTime();
     //评论页面
     Page<Comment> queryCommentByPage(int pageNo, int pageSize);
